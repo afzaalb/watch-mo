3c070c8edcac6064ed9db7c90e7882abd6e11233
afzaalbutt <afzaal@turnlunchon.com>
2018-03-23 16:04:03 +0500

    basename added

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import App from "./hoc/App";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
-    <Router>
+    <Router basename="watchMo">
         <Route component={App} />
     </Router>,
     document.getElementById("reactive-movies-base")

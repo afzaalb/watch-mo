e10a48f5c54a10a4ebdac1e27b280d604ad7e42f
afzaalbutt <afzaal@turnlunchon.com>
2018-03-23 22:45:24 +0500

    Prod

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import App from "./hoc/App";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
-    <Router basename="watchMo">
+    <Router>
         <Route component={App} />
     </Router>,
     document.getElementById("reactive-movies-base")

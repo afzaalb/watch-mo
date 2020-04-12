7aa555b49d957f01b982fc0d9f6f7790625bcacb
Afzaal B <afzaalopera@gmail.com>
2020-04-13 00:48:46 +0500

    Gallery Preview completed UI desktop

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 require("dotenv").config();
 
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(process.env.NODE_PORT, function() {
+app.listen(process.env.NODE_PORT, function () {
   console.log("Listening on port " + process.env.NODE_PORT);
 });
--- src/components/shared/gallery-modal/index.js
+++ src/components/shared/gallery-modal/index.js
@@ -1,15 +1,18 @@
 import React, { PureComponent } from "react";
+import Modal from "react-bootstrap/Modal";
 import GalleryCarousel from "./GalleryCarousel";
 import ExpandGallery from "./ExpandGallery";
 import PropTypes from "prop-types";
+import classNames from "classnames";
+import Close from "react-bytesize-icons/Close";
 
 class GalleryModal extends PureComponent {
   state = {
-    isGalleryModalShown: false
+    isGalleryModalShown: false,
   };
 
   toggleGalleryModalHandler = () => {
-    this.setState(prevState => {
+    this.setState((prevState) => {
       return { isGalleryModalShown: !prevState.isGalleryModalShown };
     });
   };
@@ -17,23 +20,38 @@ class GalleryModal extends PureComponent {
   render() {
     const { images, title, children } = this.props;
     const { isGalleryModalShown } = this.state;
+    const shouldRenderGallery = images.length > 1;
 
     return (
       <>
         <div
-          className="gallery-preview c-hand position-relative overflow-hidden"
+          className={classNames(
+            "gallery-preview position-relative overflow-hidden",
+            { "c-hand": shouldRenderGallery }
+          )}
           onClick={this.toggleGalleryModalHandler}
         >
           {children}
-          <ExpandGallery />
+          {shouldRenderGallery && <ExpandGallery />}
         </div>
-        {isGalleryModalShown && (
-          <GalleryCarousel
-            images={images}
-            title={title}
-            isGalleryModalShown={isGalleryModalShown}
-            toggleGalleryModalHandler={this.toggleGalleryModalHandler}
-          />
+        {shouldRenderGallery && (
+          <Modal
+            centered
+            backdrop={false}
+            show={isGalleryModalShown}
+            bsPrefix="gallery-modal modal"
+            scrollable={false}
+            onHide={this.toggleGalleryModalHandler}
+          >
+            <div
+              onClick={this.toggleGalleryModalHandler}
+              className="close-icon c-hand position-absolute d-flex align-items-center justify-content-center"
+            >
+              <Close width="18" height="18" />
+            </div>
+            <h2 className="title position-absolute px-3">{title}</h2>
+            <GalleryCarousel images={images} title={title} />
+          </Modal>
         )}
       </>
     );
@@ -43,7 +61,7 @@ class GalleryModal extends PureComponent {
 GalleryModal.propTypes = {
   children: PropTypes.element.isRequired,
   images: PropTypes.array.isRequired,
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
 };
 
 export default GalleryModal;

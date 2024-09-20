1e3fcb83f4703a014b0974b606e45cdc79fdcafe
Afzaal B <afzaalopera@gmail.com>
2024-09-20 18:28:07 +0500

    Changes for ENV

--- webpack.config.js
+++ webpack.config.js
@@ -4,56 +4,56 @@ const webpack = require("webpack");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
+// const envKeys = Object.keys(env).reduce((prev, next) => {
+//     prev[`process.env.${next}`] = JSON.stringify(env[next]);
+//     return prev;
+// }, {});
 
 module.exports = {
-  entry: "./src/main.js",
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  devServer: {
-    port: process.env.NODE_PORT,
-    historyApiFallback: true, // Reloads the app on last visited route (important)
-    open: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader",
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: "file-loader",
-        options: {
-          outputPath: "assets/images/",
-        },
-      },
-      {
-        test: /\.(eot|woff|ttf|otf)$/,
-        loader: "file-loader",
-        options: {
-          outputPath: "assets/fonts/",
-        },
-      },
+    entry: "./src/main.js",
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/",
+    },
+    devServer: {
+        port: process.env.NODE_PORT,
+        historyApiFallback: true, // Reloads the app on last visited route (important)
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader",
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: "file-loader",
+                options: {
+                    outputPath: "assets/images/",
+                },
+            },
+            {
+                test: /\.(eot|woff|ttf|otf)$/,
+                loader: "file-loader",
+                options: {
+                    outputPath: "assets/fonts/",
+                },
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(["dist"]),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            favicon: "./src/assets/images/icon.png",
+        }),
+        // new webpack.DefinePlugin(envKeys),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(["dist"]),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      favicon: "./src/assets/images/icon.png",
-    }),
-    new webpack.DefinePlugin(envKeys),
-  ],
 };
